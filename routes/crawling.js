4ee9229cc4aa2228e8cd2c245dd8446bbc3a68bd
qp <ts.kim@gsshop.com>
2019-03-14 19:02:50 +0900

    chore: 날짜문자열 수정

--- routes/crawling.js
+++ routes/crawling.js
@@ -14,20 +14,29 @@ const urlArr = [
   
 ];
 
+const getDateStr = () => {
+  let yearStr = new Date().getFullYear() + "" ;
+  let monthStr = ((new Date().getMonth() + 1) + "").length === 1 ? "0" + (new Date().getMonth() + 1) : (new Date().getMonth() + 1) + "";
+  let dateStr = (new Date().getDate() + "").length === 1 ? "0" + new Date().getDate() : new Date().getDate() + "";
+  let hoursStr = (new Date().getHours() + "").length === 1 ? "0" + new Date().getHours() : new Date().getHours() + "";
+  let minStr = (new Date().getMinutes() + "").length === 1 ? "0" + new Date().getMinutes() : new Date().getMinutes() + "";
+
+  return yearStr + monthStr + dateStr + hoursStr + minStr;
+}
+
 const crawler = async () => {
+  const browser = await puppeteer.launch({headless: false, args: ['--window-size=1920,1080', '--disable-notifications']});
+
   try {
     const result = [];
-
-    const cTime = new Date().getFullYear() + "" + (new Date().getMonth() + 1) + "" +  new Date().getDate() + "" + new Date().getHours() + "" + new Date().getMinutes();
-
     for (const targetUrl of urlArr) {
-      const browser = await puppeteer.launch({headless: true, args: ['--window-size=1920,1080'] });
+      
       const page = await browser.newPage();
       await page.setViewport({width: 1920,height: 1080,});
       await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
-      await page.goto(targetUrl ,{waitUntil: 'load'});
+      await page.goto(targetUrl);
       await page.waitForSelector('.col-xs-2-4.shopee-search-item-result__item');
-
+      
       const bodyHandle = await page.$('body');
       const { height } = await bodyHandle.boundingBox();
       await bodyHandle.dispose();
@@ -49,46 +58,53 @@ const crawler = async () => {
       const resultStr = await page.evaluate(() => {
         const tmpArr = [];
         const products = document.querySelectorAll('.col-xs-2-4.shopee-search-item-result__item');
+
         if (products.length > 1) {
           for(const ele of products) {
+            let yearStr = new Date().getFullYear() + "" ;
+            let monthStr = ((new Date().getMonth() + 1) + "").length === 1 ? "0" + (new Date().getMonth() + 1) : (new Date().getMonth() + 1) + "";
+            let dateStr = (new Date().getDate() + "").length === 1 ? "0" + new Date().getDate() : new Date().getDate() + "";
+            let hoursStr = (new Date().getHours() + "").length === 1 ? "0" + new Date().getHours() : new Date().getHours() + "";
+            let minStr = (new Date().getMinutes() + "").length === 1 ? "0" + new Date().getMinutes() : new Date().getMinutes() + "";
+            
             let productInfo = {};
             productInfo.url = ele.querySelector('a').href;
-            productInfo.imgUrl = ele.querySelector('._1T9dHf._3XaILN').style.backgroundImage.split('"')[1];
-            (ele.querySelector('._3BQlNg.bgXBUk')) ? productInfo.isFavorite = "Y" :  productInfo.isFavorite = "N";
+            productInfo.imgUrl = ele.querySelector('._1T9dHf._3XaILN') ? ele.querySelector('._1T9dHf._3XaILN').style.backgroundImage.split('"')[1] : 'fail to get image';
+            productInfo.isFavorite = ele.querySelector('._3BQlNg.bgXBUk') ? "Y" : "N";
             productInfo.prdNm = ele.querySelector('._1NoI8_._2gr36I').textContent;
             productInfo.prdCost = ele.querySelector('._341bF0').textContent;
             if (ele.querySelector('._18SLBt')) productInfo.soldCnt = ele.querySelector('._18SLBt').textContent;
 
             productInfo.targetUrl = window.location.href;
-            productInfo.createDt = new Date().getFullYear() + "" + (new Date().getMonth() + 1) + "" +  new Date().getDate() + "" + new Date().getHours() + "" + new Date().getMinutes();
+            productInfo.createDt = yearStr + monthStr + dateStr + hoursStr + minStr;
 
             tmpArr.push(productInfo);
           }
         }
           return tmpArr;
       });
+
       console.log("END~~~~~~~", resultStr);
       await page.close();
-      await browser.close();
-
+      
       result.push({targetUrl, resultStr});
     }
-
-    // debugger;
+    throw new Error("test");
     console.dir(result);
     
     if (result.length !== 0) {
-      const dirNm = 'cvs/result_'+cTime+'.csv';
+      const dirNm = 'cvs/result_'+getDateStr()+'.csv';
       
       fs.writeFileSync(dirNm, JSON.stringify(result));
     }
     
-
   } catch (err) {
     console.log('Error: ', err);
+  } finally {
+    await browser.close();
   }
 }
 
-schedule.scheduleJob('0 * * * *', () => {
+// schedule.scheduleJob('0 * * * *', () => {
   crawler();  
-});
+// });
