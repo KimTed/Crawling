3e372132f32c0e1e1dac8407adb3bdbd35574773
qp <ts.kim@gsshop.com>
2019-03-26 14:13:24 +0900

    fix: crawling 로직 개선

--- process/crawling.js
+++ process/crawling.js
@@ -37,7 +37,10 @@ const crawler = async () => {
       await page.setViewport({width: 1920,height: 1080,});
       await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
       await page.goto(targetUrl);
-      await page.waitForSelector('.col-xs-2-4.shopee-search-item-result__item');
+      
+      await page.waitForSelector('.shopee-page-wrapper');
+      // await page.waitFor(4);
+      // await page.waitForResponse();
       
       const bodyHandle = await page.$('body');
       const { height } = await bodyHandle.boundingBox();
@@ -70,11 +73,11 @@ const crawler = async () => {
             let minStr = (new Date().getMinutes() + "").length === 1 ? "0" + new Date().getMinutes() : new Date().getMinutes() + "";
             
             let productInfo = {};
-            productInfo.url = ele.querySelector('a').href;
-            productInfo.imgUrl = ele.querySelector('._1T9dHf._3XaILN') ? ele.querySelector('._1T9dHf._3XaILN').style.backgroundImage.split('"')[1] : 'fail to get image';
+            productInfo.url = ele.querySelector('a') ? ele.querySelector('a').href : 'Fail to get product url';
+            productInfo.imgUrl = ele.querySelector('._1T9dHf._3XaILN') ? ele.querySelector('._1T9dHf._3XaILN').style.backgroundImage.split('"')[1] : 'Fail to get product Image';
+            productInfo.prdNm = ele.querySelector('._1NoI8_._2gr36I') ? ele.querySelector('._1NoI8_._2gr36I').textContent : 'Fail to get product name';
             productInfo.isFavorite = ele.querySelector('._3BQlNg.bgXBUk') ? "Y" : "N";
-            productInfo.prdNm = ele.querySelector('._1NoI8_._2gr36I').textContent;
-            productInfo.originalPrdCost = (ele.querySelector('._1w9jLI.QbH7Ig.t-e-Bx')) ? ele.querySelector('._1w9jLI.QbH7Ig.t-e-Bx').textContent.replace(/[^\.0-9]/g,"") : "";
+            productInfo.originalPrdCost = ele.querySelector('._1w9jLI.QbH7Ig.t-e-Bx') ? ele.querySelector('._1w9jLI.QbH7Ig.t-e-Bx').textContent.replace(/[^\.0-9]/g,"") : "";
             
             let costArr = ele.querySelectorAll('._341bF0');
             if (costArr) {
@@ -85,7 +88,7 @@ const crawler = async () => {
                   productInfo.toPrdCost = ele.textContent.replace(/[^\.0-9]/g,"");
               });
             }
-            productInfo.soldCnt = (ele.querySelector('._18SLBt')) ? ele.querySelector('._18SLBt').textContent.replace(/[^0-9]/g,"") : "";
+            productInfo.soldCnt = ele.querySelector('._18SLBt') ? ele.querySelector('._18SLBt').textContent.replace(/[^0-9]/g,"") : "";
             productInfo.targetUrl = window.location.href;
             productInfo.createDt = yearStr + monthStr + dateStr + hoursStr + minStr;
 
@@ -94,15 +97,13 @@ const crawler = async () => {
         }
           return tmpArr;
       });
-
-      // console.log("END~~~~~~~", productArr);
+      console.log("Single Prd: ", productArr);
       
       await page.close();
-      
       result.push({targetUrl, productArr});
     }
     // throw new Error("test");
-    // console.dir(result);
+    console.dir("final: ", result);
     
     if (result.length !== 0) {
       const dirNm = 'cvs/result_'+getDateStr()+'.csv';
@@ -118,6 +119,6 @@ const crawler = async () => {
   }
 }
 
-schedule.scheduleJob('0 * * * *', () => {
+// schedule.scheduleJob('0 * * * *', () => {
   crawler();  
-});
+// });
