7f9f1fd80bdb1a205bf354e692e15ace71427aae
qp <ts.kim@gsshop.com>
2019-04-05 20:27:00 +0900

    fix: 엑셀 변환 작업 수정

--- process/chgCvsToExcel.js
+++ process/chgCvsToExcel.js
@@ -18,7 +18,9 @@ const forthArr = [];
 
 const excelColumns = [
 {header: '상품명', key: 'prdNm'},
-{header: '가격', key: 'prdCost'},
+{header: '최소가격', key: 'fromPrdCost'},
+{header: '최대가격', key: 'toPrdCost'},
+{header: '원가격', key: 'originalPrdCost'},
 {header: 'URL', key: 'url'},
 {header: '판매량', key: 'soldCnt'},
 {header: 'Favorite', key: 'isFavorite'},
@@ -27,46 +29,23 @@ const excelColumns = [
 ];
 
 try {
+    const workbook = new excel.Workbook();
     for (let fileNm of files) {
         // let fileInfo = fs.statSync(path.join(fileDir, fileNm));
         let fileData = fs.readFileSync(path.join(fileDir, fileNm), 'utf-8');
         let fileDataJson = JSON.parse(fileData);
+        
         console.log(fileDataJson);
         fileDataJson.some((ele, idx) => {
-            if (idx === 0 ) ele.resultStr.map(ele => firstArr.push(ele));
-            else if (idx === 1 ) ele.resultStr.map(ele => secondArr.push(ele));
-            else if (idx === 2 ) ele.resultStr.map(ele => thirdArr.push(ele));
-            else if (idx === 3 ) ele.resultStr.map(ele => forthArr.push(ele));
+            
+            let worksheet = workbook.addWorksheet(ele.targetUrl);
+            worksheet.columns = excelColumns;
+            ele.productArr.forEach(ele => {
+                worksheet.addRow(ele);
+            });
         });
     }
 
-    console.log(firstArr)
-    console.log(secondArr)
-    console.log(thirdArr)
-    console.log(forthArr)
-
-    const workbook = new excel.Workbook();
-    const worksheet0 = workbook.addWorksheet('');
-    const worksheet1 = workbook.addWorksheet('');
-    const worksheet2 = workbook.addWorksheet('');
-    const worksheet3 = workbook.addWorksheet('');
-    worksheet0.columns = excelColumns;
-    worksheet1.columns = excelColumns;
-    worksheet2.columns = excelColumns;
-    worksheet3.columns = excelColumns;
-
-    firstArr.forEach(ele => {
-        worksheet0.addRow(ele);
-    });
-    secondArr.forEach(ele => {
-        worksheet1.addRow(ele);
-    });
-    thirdArr.forEach(ele => {
-        worksheet2.addRow(ele);
-    });
-    forthArr.forEach(ele => {
-        worksheet3.addRow(ele);
-    });
 
     workbook.xlsx.writeFile("shopee.xlsx").then(function() {
         // done
